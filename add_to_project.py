a1dae9485188703718f064c9ecb4758093b503d2
Jace Ju <jaceju@gmail.com>
2015-04-21 12:46:10 +0800

    Add folder that contains current file to sidebar

---
+++ add_to_project.py
@@ -0,0 +1,28 @@
+import sublime
+import sublime_plugin
+import os.path
+
+# Add folder that contains current file to sidebar.
+class AddToProjectCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		path = sublime.active_window().active_view().file_name()
+		if path == None:
+			return
+		path = os.path.dirname(path)
+
+		# TODO: Try to see if it's possible to preserve expanded folders when adding a new folder.
+		#wat = sublime.active_window().active_view().settings().has("expanded_folders")
+		#if wat:
+		#	sublime.message_dialog("true")
+		#else:
+		#	sublime.message_dialog("false")
+
+		data = sublime.active_window().project_data()
+		if data:
+			for folder in data['folders']:
+				if folder['path'] == path:
+					return
+			data['folders'].append({'follow_symlinks': True, 'path': path})
+		else:
+			data = {'folders': [{'follow_symlinks': True, 'path': path}]}
+		sublime.active_window().set_project_data(data)
